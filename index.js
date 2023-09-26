7b2bc1b404f497237c14c5d624fe4d937984586c
Michael Friedman <minorpianokeys@gmail.com>
2023-09-26 12:49:39 -0400

    fixed bug

--- index.js
+++ index.js
@@ -3,35 +3,39 @@ document.addEventListener('DOMContentLoaded', function() {
     const newPokeBtn = document.querySelector("#new")
     const pokeContainer = document.querySelector("#pokemonContainer")
 
+
     newPokeBtn.addEventListener('click', function() {
         handleNewPoke()
     })
+
+    //Handle Guess
+    guessForm.addEventListener('submit', function(e) {
+        e.preventDefault()
+        const userGuess = guessForm.guess.value.toLowerCase();
+        if(userGuess === pokeName) {
+            console.log("correct")
+        } else {
+            console.log("incorrect")
+        }  
+    })
+
+    let pokeName;
     
     function handleNewPoke() {
+        
         //Fetch Pokemon
-        const randomPokeNum = Math.floor(Math.random() * 151)
+        const randomPokeNum = Math.floor(Math.random() * 150 + 1)
         fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokeNum}`)
         .then(res => res.json())
         .then(function(data) {
-            const pokeName = data.name;
+            pokeName = data.name;
             const pokeImage = data.sprites.other["official-artwork"].front_default;
-            
+            console.log(pokeName)
+
             //Create Card
             pokeContainer.innerHTML = `
             <img src="${pokeImage}">
             `
-
-            //Handle Form
-            guessForm.addEventListener('submit', function(e) {
-                e.preventDefault()
-                const userGuess = guessForm.guess.value.toLowerCase();
-                console.log(userGuess)
-                if(userGuess == pokeName) {
-                    console.log("correct")
-                } else {
-                    console.log("incorrect")
-                }
-            })  
-        })
-    }  
+        })      
+    }
 })
